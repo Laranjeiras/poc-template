052c6284a3a835ecb04e287309b72be19f49cf30
Carlos Alexandre Laranjeiras <laranja22@hotmail.com>
2025-02-15 22:04:08 -0300

    refactor: altera banco de dados para InMemoryDb
    - Remove dependência do SQL Server
    - Adiciona pacote Microsoft.EntityFrameworkCore.InMemory
    - Configura DbContext para usar banco de dados em memória

--- src/ROJINE.IoC/DependencyInjection.cs
+++ src/ROJINE.IoC/DependencyInjection.cs
@@ -13,7 +13,8 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            options.UseInMemoryDatabase("RojineInMemoryDb")
+        );
 
         services.AddScoped<IProdutoRepository, ProdutoRepository>();
         services.AddScoped<CriarProdutoUseCase>();
